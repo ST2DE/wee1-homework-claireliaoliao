32c9c23de5d272f735327973913d1df997177f4d
claire <cla022318@yahoo.com.tw>
2018-04-14 12:12:04 +0800

    add express

--- express/express.js
+++ express/express.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express(); //建立一個Express伺服器
+
+app.set('view engine', 'ejs');
+
 app.listen(3000); //告訴server聽取3000這個Port
 
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+app.get('/about_me', function(req, res) {
+    res.render('about_me', { name: req.query.name });
+});
