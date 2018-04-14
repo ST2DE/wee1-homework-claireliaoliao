be9ddffd674c9190bc00428d88e36e395fa9a42c
claire <cla022318@yahoo.com.tw>
2018-04-14 12:20:16 +0800

    express.js

--- express/express.js
+++ express/express.js
@@ -5,9 +5,6 @@ app.set('view engine', 'ejs');
 
 app.listen(3000); //告訴server聽取3000這個Port
 
-app.get('/', function(req, res) {
-    res.send('hello');
-});
 
 app.get('/about_me', function(req, res) {
     res.render('about_me', { name: req.query.name });
